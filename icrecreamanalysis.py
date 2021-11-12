efeaa6592587c15dd1d8bd50a9d4ba7bfc558d63
Naicker Creason <naickercreason@zipcodes-MacBook-Pro-7.local>
2021-11-12 15:37:49 -0500

    -_-

--- icrecreamanalysis.py
+++ icrecreamanalysis.py
@@ -2,3 +2,25 @@
 # Analysis if the ice cream dataset.
 #
 
+import csv
+
+
+file = open('ice_cream.csv', 'r')
+
+type(file)
+
+csvreader = csv.reader(file)
+
+start = next(csvreader)
+print(start)
+
+rows = []
+
+for row in csvreader:
+    rows.append(row)
+print(rows)
+file.close()
+
+#I cant get this to format properly.....uhg
+
+
